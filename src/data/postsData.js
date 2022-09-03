fd3e87753e1d14c482ef8d292d1d94861c535465
nonfermata <nonfermata@gmail.com>
2022-09-04 00:54:09 +0400

    improve dialogs

---
+++ src/data/postsData.js
@@ -0,0 +1,76 @@
+import React from "react";
+import avatar from './assets/avatar.png'
+
+export const dialogsData = [
+    {
+        id: '1',
+        name: 'John',
+        avaURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEqDEIPAxaIU5pwkbs7CiS29TVmrBk2a5wBQ&usqp=CAU',
+    },
+    {
+        id: '2',
+        name: 'Paul',
+        avaURL: 'https://lefthanderslegacy.org/wp-content/uploads/2018/07/Paul-McCartney.jpg',
+    },
+    {
+        id: '3',
+        name: 'George',
+        avaURL: 'https://iscale.iheart.com/catalog/artist/29143',
+    },
+    {
+        id: '4',
+        name: 'Ringo',
+        avaURL: 'https://d2kdkfqxnvpuu9.cloudfront.net/images/big/74628.jpg?1475075592',
+    },
+    {
+        id: '5',
+        name: 'Dolores',
+        avaURL: 'https://www.gala.fr/imgre/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2Fgal.2Fvar.2Fgal.2Fstorage.2Fimages.2Fmedia.2Fmultiupload_du_10_novembre_2014.2Fdolores-o_riordan.2F3170340-1-fre-FR.2Fdolores-o_riordan.2Ejpg/480x480/quality/80/dolores-o-riordan-a-perdu-pied.jpg',
+    },
+    {
+        id: '6',
+        name: 'Sting',
+        avaURL: 'https://i.discogs.com/0SNA0qJGLPxqLjNlPYtFQ06TpithNK3jLUZaP98tpXE/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTU4Njk4/MC0xMTczMzg3OTg4/LmpwZWc.jpeg',
+    },
+    {
+        id: '7',
+        name: 'Suzanne',
+        avaURL: 'https://www.eventworld.co/blob/images/pg/suzanne-vega_b60e0_500.jpg',
+    },
+]
+
+export const user = {
+    avatar: avatar,
+    name: "Dmitry Baulin",
+    dateOfBirth: "10.09.1975",
+    city: "Moscow",
+    website: "https://febrecords.ru",
+    phone: "+7 123 4567890"
+}
+
+export const postsData = [
+    {
+        id: '1',
+        avaURL: 'https://static01.nyt.com/newsgraphics/2020/11/12/fake-people/4b806cf591a8a76adfc88d19e90c8c634345bf3d/fallbacks/mobile-05.jpg',
+        postText: 'Amen lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque fugiat veritatis rem sequi sed velit id illo provident repellendus nihil! Harum, velit doloremque quibusdam minus dolore commodi aut iusto architecto! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque fugiat veritatis rem sequi sed velit id illo provident repellendus nihil! Harum, velit doloremque quibusdam minus dolore commodi aut iusto architecto!',
+        numberOfLikes: '20',
+    },
+    {
+        id: '2',
+        avaURL: 'https://pbs.twimg.com/media/ES1asUFWkAE1PLw.jpg',
+        postText: 'Bit amet consectetur adipisicing elit, aliquid similique nulla perferendis ullam dolorum ab. Quos adipisci alias dolor maxime id, assumenda aperiam, quibusdam saepe, modi iusto aspernatur fugiat tempore officia repellendus? Quasi commodi maiores minus tenetur suscipit delectus, nisi in soluta. Harum recusandae atque voluptas exercitationem ex odio dolor voluptatibus perferendis quisquam tenetur velit molestias voluptatem totam, laboriosam amet ad quae a nihil nemo dicta? Eius, voluptates molestias fugiat obcaecati omnis perferendis animi nobis voluptatem vitae necessitatibus quas modi quo libero culpa laboriosam possimus consequuntur facere deserunt repudiandae consectetur. Quaerat esse illum nemo sint, minima placeat reiciendis totam incidunt sunt?',
+        numberOfLikes: '12',
+    },
+    {
+        id: '3',
+        avaURL: 'https://www.thesun.co.uk/wp-content/uploads/2019/02/Comp31.jpg',
+        postText: 'Cumque fugiat veritatis rem sequi sed velit id illo provident repellendus nihil! Harum, velit doloremque quibusdam minus dolore commodi aut iusto architecto! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque fugiat veritatis rem sequi sed velit id illo provident repellendus nihil! Harum, velit doloremque quibusdam minus dolore commodi aut iusto architecto!',
+        numberOfLikes: '24',
+    },
+    {
+        id: '4',
+        avaURL: 'https://static01.nyt.com/newsgraphics/2020/11/12/fake-people/4b806cf591a8a76adfc88d19e90c8c634345bf3d/fallbacks/mobile-07.jpg',
+        postText: 'Dolor sit amet consectetur adipisicing elit. Id inventore quasi cum at fugiat magni laboriosam exercitationem eum quo ipsa non sint molestias numquam, obcaecati dignissimos corporis. Minima, cumque blanditiis nulla provident ducimus magnam, quisquam tempora architecto esse mollitia aperiam consequatur explicabo itaque sequi quos quae a veritatis pariatur facilis voluptatem iste exercitationem id est! Rerum deleniti tenetur, eveniet autem hic ipsa adipisci pariatur magni ipsam neque. Explicabo adipisci blanditiis deserunt asperiores, exercitationem eligendi quisquam obcaecati iusto.',
+        numberOfLikes: '8',
+    },
+]
